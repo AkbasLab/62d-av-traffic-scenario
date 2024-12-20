25222aa8bb85cc46322bc39dbd3295d7033b68e4
PATEW <claypate784@gmail.com>
2024-12-19 23:49:47 -0500

    trained, needs final data

--- explainability/app.py
+++ explainability/app.py
@@ -4,7 +4,6 @@ from shap_ex import ShapAnalyzer
 import numpy as np
 import pandas as pd
 import os
-#from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from pycaret import regression
 
